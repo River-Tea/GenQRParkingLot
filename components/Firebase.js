d1207c014c82cbaa2f107e802125723c66b0278a
River-Tea <tragiang.private@gmail.com>
2023-06-03 21:11:09 +0700

    config Firebase

--- components/Firebase.js
+++ components/Firebase.js
@@ -1,22 +1,21 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
-
-// Initialize Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
+  apiKey: "AIzaSyDwEucGJIQgC5YvKECYkvjlImIJ9oiOSzA",
+  authDomain: "genqrparking.firebaseapp.com",
+  projectId: "genqrparking",
+  storageBucket: "genqrparking.appspot.com",
+  messagingSenderId: "251439008785",
+  appId: "1:251439008785:web:b0eb34e0f8b59b3b8df5b0",
+  measurementId: "G-BJVJTZWFXQ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
